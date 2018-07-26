2bda322ced17dd9978fdcdff3a7a463faa6b60b3
Catalin <ene.catalinionut@yahoo.com>
2018-07-26 17:07:37 +0300

    Menu button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+//Select DOM items
+const $menuBtn = document.querySelector(".menu-btn");
+const $menu = document.querySelector(".menu");
+const $menuBrand = document.querySelector(".menu-branding");
+const $menuNav = document.querySelector(".menu-nav");
+const $navItems = document.querySelectorAll(".nav-item");
+
+//set initial stat of menu
+let showMenu = false;
+const toggleMenu = _ => {
+  if (!showMenu) {
+    $menuBtn.classList.add("close");
+    $menu.classList.add("show");
+    $menuNav.classList.add("show");
+    $menuBrand.classList.add("show");
+    $navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    showMenu = false;
+    $menuBtn.classList.remove("close");
+    $menu.classList.remove("show");
+    $menuNav.classList.remove("show");
+    $menuBrand.classList.remove("show");
+    navItem.forEach(item => item.classList.remove("show"));
+  }
+};
+
+$menuBtn.addEventListener("click", toggleMenu);
